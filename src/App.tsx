5f1765f37ca91437ea3afb58cd29e17f68b3a089
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-05-10 03:41:05 -0300

    TableArea Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react'
 import * as C from './App.styles'
+import { Category } from './types/Category'
+import { Item } from './types/Item'
+import { categories } from './data/categories'
+import { items } from './data/items'
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter'
+import { TableArea } from './components/TableArea'
+import { InfoArea } from './components/InfoArea'
+
+
 
 const App = () => {
+
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+    setFilteredList( filterListByMonth(list, currentMonth) );
+  }, [list, currentMonth]);
+
   return(
     <C.Container>
       <C.Header>
@@ -8,11 +27,11 @@ const App = () => {
       </C.Header>
       <C.Body>
         
-        {/* Area de informaçoes */}
+        <InfoArea currentMonth={currentMonth}  />
 
         {/* Area de inserção */}
 
-        {/* Area de itens */}
+        <TableArea list={filteredList} />
 
       </C.Body>
     </C.Container>
